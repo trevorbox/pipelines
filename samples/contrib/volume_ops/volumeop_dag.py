2382661b0f7ab671cfad4304dce9e970f6b6c904
trevorbox <trevorjbox@gmail.com>
2021-05-25 12:03:36 -0700

    emptydir

--- samples/contrib/volume_ops/volumeop_dag.py
+++ samples/contrib/volume_ops/volumeop_dag.py
@@ -21,35 +21,33 @@ from kubernetes import client as k8s_client
     description="The second example of the design doc."
 )
 def volume_op_dag():
-    vop = dsl.VolumeOp(
-        name="create_pvc",
-        resource_name="my-pvc",
-        size="10Gi",
-        modes=dsl.VOLUME_MODE_RWM
-    )
+
+    volume = dsl.PipelineVolume(volume=k8s.client.V1Volume(
+      name="test-storage",
+      empty_dir=k8s.client.V1EmptyDirVolumeSource()))
 
     step1 = dsl.ContainerOp(
         name="step1",
         image="library/bash:4.4.23",
         command=["sh", "-c"],
         arguments=["echo 1 | tee /mnt/file1"],
-        pvolumes={"/mnt": vop.volume}
+        pvolumes={"/mnt": volume}
     )
 
     step2 = dsl.ContainerOp(
         name="step2",
         image="library/bash:4.4.23",
         command=["sh", "-c"],
-        arguments=["echo 2 | tee /mnt2/file2"],
-        pvolumes={"/mnt2": vop.volume}
+        arguments=["echo 2 | tee /mnt/file2"],
+        pvolumes={"/mnt": step1.volume}
     )
 
     step3 = dsl.ContainerOp(
         name="step3",
         image="library/bash:4.4.23",
         command=["sh", "-c"],
-        arguments=["cat /mnt/file1 /mnt/file2"],
-        pvolumes={"/mnt": vop.volume.after(step1, step2)}
+        arguments=["echo 3 | tee /mnt/file3"],
+        pvolumes={"/mnt": volume.after(step1, step2)}
     )
 
     dsl.get_pipeline_conf().set_image_pull_secrets([k8s_client.V1ObjectReference(name="secretA")])
